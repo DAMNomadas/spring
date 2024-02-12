49ce5c6a18caea6f217ba157799c4fde1e6b8f31
jtorrast <jordi.torras89@gmail.com>
2024-02-12 12:55:03 +0100

    Añadido repositorio ciudad

---
+++ Wanderer/src/main/java/org/nomad/wanderer/repository/ICiudadRepository.java
@@ -0,0 +1,14 @@
+package org.nomad.wanderer.repository;
+
+import org.nomad.wanderer.model.Ciudad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ICiudadRepository extends JpaRepository<Ciudad, Integer> {
+
+    Ciudad getCiudadByNombreCiudad(String nombreCiudad);
+
+}
